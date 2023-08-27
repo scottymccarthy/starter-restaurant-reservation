1db90d95b96d39468e0672a15686599f7f594668
Scotty McCarthy <119365979+scottymccarthy@users.noreply.github.com>
2023-08-27 10:13:47 -0600

    Update 20210123122225_createReservationsTable.js

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name");
+    table.string("last_name");
+    table.string("mobile_number");
+    table.string("people");
+    table.date("reservation_date");
+    table.time("reservation_time");
     table.timestamps(true, true);
   });
 };
@@ -8,3 +14,6 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTable("reservations");
 };
+
+
+//The reservation table displaying all information that will be stored in our back-end and then displayed on our webpage.
