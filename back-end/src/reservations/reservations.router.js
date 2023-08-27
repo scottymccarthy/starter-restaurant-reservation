06d55d8b3ee2cd342e5cb5bb4ae3f1ef9454aa49
Scotty McCarthy <119365979+scottymccarthy@users.noreply.github.com>
2023-08-27 10:22:32 -0600

    Update reservations.router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -5,8 +5,27 @@
  */
 
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./reservations.controller");
 
-router.route("/").get(controller.list);
+
+router
+.route("/:reservation_Id/status")
+.put(controller.update)
+.all(methodNotAllowed);
+
+
+router
+.route("/:reservation_Id")
+.get(controller.read)
+.put(controller.modify)
+.all(methodNotAllowed);
+
+
+router
+.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed);
 
 module.exports = router;
